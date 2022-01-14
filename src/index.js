9c9294daf2a3adc4d47e25254322648637e0cd18
Will <willyknapp@gmail.com>
2022-01-14 17:39:06 -0500

    update react Routes to reflect github pages route

--- src/index.js
+++ src/index.js
@@ -18,10 +18,11 @@ ReactDOM.render(
     <BrowserRouter>
     <Navbar />
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/portfolio" element={<Portfolio />} />
-        <Route path="/about" element={<About />} />
+        <Route path="/ReactSite" element={<Home />} />
+        <Route path="/ReactSite/contact" element={<Contact />} />
+        <Route path="/ReactSite/portfolio" element={<Portfolio />} />
+        <Route path="/ReactSite/about" element={<About />} />
+        <Route path="/ReactSite/*" element={<FourOhFour />} />
         <Route path="/*" element={<FourOhFour />} />
       </Routes>
     </BrowserRouter>
