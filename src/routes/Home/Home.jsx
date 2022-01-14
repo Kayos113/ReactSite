ff35e2e0ec70ac4f1b7700a49ccf5844c6b13a6e
Will <willyknapp@gmail.com>
2022-01-14 18:09:34 -0500

    Home Text Update

--- src/routes/Home/Home.jsx
+++ src/routes/Home/Home.jsx
@@ -6,7 +6,7 @@ function Heading() {
   <div className="heading">
     <div className="text-display">
       <h1 className="nametag">William Knapp</h1>
-      <p className="title">Web and Fullstack Developer</p>
+      <p className="title">Java and Web Developer</p>
       <a className="btn" href="contact">Hire Me</a>
     </div>
   </div>);
